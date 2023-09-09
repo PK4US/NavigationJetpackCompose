c4dbee2022f45feb00e2a76be2b7e9863fe5bce4
PK4US <stepanchuk.ei@gmail.com>
2023-09-09 15:06:24 +0300

    HelloApp

--- app/src/main/java/com/pk4us/navigationjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/navigationjetpackcompose/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.pk4us.navigationjetpackcompose.ui.theme.NavigationJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-
+                    Text(text = "Hello App")
 
                 }
             }
