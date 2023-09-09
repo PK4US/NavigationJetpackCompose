c85863c4f0cc0cce032c8a76c55113dc7fdb7b96
PK4US <stepanchuk.ei@gmail.com>
2023-09-09 15:14:42 +0300

    Create Screen Composables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "com.pk4us.navigationjetpackcompose"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.pk4us.navigationjetpackcompose"
         minSdk = 26
-        targetSdk = 33
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/pk4us/navigationjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/pk4us/navigationjetpackcompose/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    Text(text = "Hello App")
+
 
                 }
             }
